2f4bda569815bcac0e2bf5733232a5637940dc96
Shaheer Farooqi <shaheerweboryx1@gmail.com>
2021-10-30 01:44:03 +0500

    q4

---
+++ q4.js
@@ -0,0 +1,18 @@
+function adjacentElementsProduct(inputArray) {
+    var arr = inputArray;
+    var x = 0;
+    var y = 0;
+    var p = Number.MIN_SAFE_INTEGER;
+    for (var i = 0; i < arr.length; i++) {
+      x = arr[i];
+      y = arr[i + 1];
+      if (x * y > p) {
+        p = x * y;
+        console.log(x,y);
+        console.log(p);
+      };
+    };
+    return p;
+  };
+  
+  (adjacentElementsProduct([3,6,-2,-5,7,3]));
