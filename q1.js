772603861a985329f798c658ea75cc6d264d1895
Shaheer Farooqi <shaheerweboryx1@gmail.com>
2021-11-03 20:10:03 +0500

    Update q1.js

--- q1.js
+++ q1.js
@@ -1,6 +1,7 @@
-arr = [1, 2, 3, 4];
-arr1 = [];
+var arr =[1,2,3,4,5];
+let arr1 = [];
 for (let i = arr.length - 1; i >= 0; i--) {
-    arr1.push(arr[i]);
-}
-console.log(arr1);
+    arr1.push(arr[i]);}
+arr=arr1;
+console.log(arr);
+
